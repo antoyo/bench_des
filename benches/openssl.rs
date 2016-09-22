f2cd2c601b0940388297fd8e65cb21b7342c6726
Antoni Boucher <bouanto@zoho.com>
2016-09-22 12:50:48 -0400

    Added nopad option.

--- benches/openssl.rs
+++ benches/openssl.rs
@@ -5,7 +5,8 @@ extern crate test;
 
 use test::Bencher;
 
-use openssl::crypto::symm::encrypt;
+use openssl::crypto::symm::{Crypter, encrypt};
+use openssl::crypto::symm::Mode::Encrypt;
 use openssl::crypto::symm::Type::DES_ECB;
 
 #[bench]
@@ -13,6 +14,10 @@ fn bench_decrypt(bencher: &mut Bencher) {
     let key = [0x13, 0x34, 0x57, 0x79, 0x9B, 0xBC, 0xDF, 0xF1];
     let message = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF];
     bencher.iter(|| {
-        encrypt(DES_ECB, &key, None, &message)
+        let mut crypter = Crypter::new(DES_ECB, Encrypt, &key, None).unwrap();
+        crypter.pad(false);
+        let mut result = vec![0; 16];
+        crypter.update(&message, &mut result);
+        crypter.finalize(&mut result).unwrap();
     });
 }
