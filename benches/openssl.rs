b80111042f8ea002f3d5f69b9daeef71d2ef4b21
Antoni Boucher <bouanto@zoho.com>
2016-09-22 12:52:43 -0400

    Moved the crypter creation outside the benchmark.

--- benches/openssl.rs
+++ benches/openssl.rs
@@ -13,9 +13,9 @@ use openssl::crypto::symm::Type::DES_ECB;
 fn bench_decrypt(bencher: &mut Bencher) {
     let key = [0x13, 0x34, 0x57, 0x79, 0x9B, 0xBC, 0xDF, 0xF1];
     let message = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF];
+    let mut crypter = Crypter::new(DES_ECB, Encrypt, &key, None).unwrap();
+    crypter.pad(false);
     bencher.iter(|| {
-        let mut crypter = Crypter::new(DES_ECB, Encrypt, &key, None).unwrap();
-        crypter.pad(false);
         let mut result = vec![0; 16];
         crypter.update(&message, &mut result);
         crypter.finalize(&mut result).unwrap();
